3582ce65e51735dfa11a7c964333f6c0fb20904c
Kartik Taak <kartiktaak@Kartiks-MacBook-Pro.local>
2026-01-27 19:08:17 +0530

    push_BasicScreen

--- App.jsx
+++ App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { enableScreens } from 'react-native-screens';
 import RootNavigator from './src/navigation/RootNavigator';
 import './global.css';
 
+enableScreens();
+
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle="dark-content" />
-    </SafeAreaProvider>
+        <RootNavigator />
   );
 }
 
--- src/navigation/RootNavigator.jsx
+++ src/navigation/RootNavigator.jsx
@@ -1,13 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AuthStack from './AuthStack';
 import AppStack from './AppStack';
 
  const RootNavigator = () => {
   const isLoggedIn = false; 
   return (
-    <NavigationContainer>
-      {isLoggedIn ? <AppStack /> : <AuthStack />}
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        {isLoggedIn ? <AppStack /> : <AuthStack />}
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 export default RootNavigator
